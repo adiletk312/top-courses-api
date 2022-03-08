51e1486b21edb2d8fe4dfc922944c2d7c3628647
Adilet K <adiletk312@gmail.com>
2022-03-08 15:15:29 +0600

    Feat: Implement validation pipes

--- test/review.e2e-spec.ts
+++ test/review.e2e-spec.ts
@@ -40,6 +40,13 @@ describe('AppController (e2e)', () => {
       });
   });
 
+  it('/review/create (POST) - Fail', () => {
+    return request(app.getHttpServer())
+      .post('/review/create')
+      .send({ ...testDto, rating: 0 })
+      .expect(400);
+  });
+
   it('/review/byProduct/:productId (GET) - Success', async () => {
     return request(app.getHttpServer())
       .get('/review/byProduct/' + productId)
